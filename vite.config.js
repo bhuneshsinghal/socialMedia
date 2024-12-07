8a44911e2df7a5e324c417e0409ff82d922ff14f
bhuneshsinghal <bhuneshsinghal1@gmail.com>
2024-12-07 22:46:39 +0530

    add in vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/socialMedia/"
+  base: "/socialMedia/",
+  optimizeDeps: {
+    include: ['react-icons'],
+  },
 })
